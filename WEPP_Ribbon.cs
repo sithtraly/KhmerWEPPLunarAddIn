7580c23e85cbbd0b4338baa5cf9ee26c397f7cd8
Traly Sith <sithtraly147@gmail.com>
2026-06-09 13:42:24 +0700

    Add project files.

---
+++ WEPP_Ribbon.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.Office.Tools;
+using Microsoft.Office.Tools.Ribbon;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace KhmerWEPPLunarAddIn
+{
+	public partial class WEPP_Ribbon
+	{
+		private void WEPP_Ribbon_Load(object sender, RibbonUIEventArgs e)
+		{
+
+		}
+
+		private void BtOpenSidepane_Click(object sender, RibbonControlEventArgs e)
+		{
+			Globals.ThisAddIn.taskpane.Visible = true;
+		}
+	}
+}
